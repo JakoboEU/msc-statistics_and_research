5d7ee262bb1edfc6f459b657ccdbb075a36ba14b
JakoboEU <james@jakobo.eu>
2019-02-28 12:49:57 +0000

    Add layout for assessment

---
+++ assessment/james_richardson_assessment.R
@@ -0,0 +1,102 @@
+# Date: 05/03/2019 
+# Name: James Richardson
+# Student Number: 18057447
+
+# Script Set Up
+# -------------
+# clear r console
+rm(list=ls())
+# set working directory
+setwd("/Users/jamese.richardson/Projects/Masters/statistics_and_research/assessment")
+# list files in working directory
+list.files() 
+
+# Load required libaries
+library(tidyverse)
+library(ggplot2)
+
+# In questions where you are asked to analyse data, you must follow the analysis pathways set out in the lectures, e.g.:
+#   1. Write clear hypotheses
+#   2. Explore descriptive statistics – means, standard deviation, distribution etc
+#   3. Create appropriate plots to visualise/explore data
+#   4. Decide on test(s) to be done (see flow chart)
+#   5. Examine assumptions of tests: normality, variances, independence
+#   6. Carry out tests
+#   7. Report your results: results statements / plots
+
+
+#################
+# Section 1
+#################
+
+
+# Null Hypothesis:
+# ----------------
+#
+
+
+# Results Statement:
+# ------------------
+#
+
+
+#################
+# Section 2
+#################
+
+
+# Null Hypothesis:
+# ----------------
+#
+
+
+# Results Statement:
+# ------------------
+#
+
+#################
+# Section 3
+#################
+
+
+
+# Null Hypothesis:
+# ----------------
+#
+
+
+# Results Statement:
+# ------------------
+#
+
+
+#################
+# Section 4
+#################
+
+
+# Null Hypothesis:
+# ----------------
+#
+
+
+# Results Statement:
+# ------------------
+#
+
+#################
+# Section 5
+#################
+
+
+# Null Hypothesis:
+# ----------------
+#
+
+
+# Results Statement:
+# ------------------
+#
+
+
+
